21f3ad463f437c3c3d444cdd8009703936a53cc7
Charles Varley <2391564V@student.gla.ac.uk>
2019-02-02 16:22:30 +0000

    Chapter 8
    Started chapter 8

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from rango import views
 
+app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     #url(r'index/', views.index, name='index'),
@@ -8,6 +9,6 @@ urlpatterns = [
     url(r'category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='category'),
     url(r'add_category/$', views.add_category, name='add_category'),
-    url(r'category/(?P<category_name_slug>[\w\-]+)/add_page/$', 
+    url(r'category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page, name='add_page')
 ]

32eb1d3545ea2ef343672959af7d77d1b7ce5585
2391564V <46708548+2391564V@users.noreply.github.com>
2019-02-08 15:33:12 +0000

    Chapter 10 - Bypass 2
    Again

--- rango/urls.py
+++ rango/urls.py
@@ -12,7 +12,5 @@ urlpatterns = [
     url(r'category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page, name='add_page'),
     url(r'register/$', views.register, name='register'),
-    url(r'user_login/$', views.user_login, name='user_login'),
-    url(r'user_logout/$', views.user_logout, name='user_logout'),
-    url(r'restricted/$', views.restricted, name='restricted'),
+    url(r'user_login/&', views.user_login, name='user_login'),
 ]
